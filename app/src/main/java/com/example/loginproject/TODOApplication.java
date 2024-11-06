28d05eda3a618f371d53d9f5b1e1cb86167479f4
AlexisG0819 <alexxis.gutierrez.2017@gmail.com>
2024-11-06 12:17:09 -0600

    Guia 6

---
+++ app/src/main/java/com/example/loginproject/TODOApplication.java
@@ -0,0 +1,11 @@
+package com.example.loginproject;
+
+import android.app.Application;
+
+public class TODOApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Inicializa aquí cualquier cosa que necesites para tu aplicación
+    }
+}
